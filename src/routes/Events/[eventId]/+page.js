d1a6b7966474044234cc254385a102fc08a37c57
kallul <gogoikallul33@gmail.com>
2025-06-18 23:17:01 +0530

    commited

---
+++ src/routes/Events/[eventId]/+page.js
@@ -0,0 +1,12 @@
+// eventID
+export const load = async ({ fetch }) => {
+	try {
+		const response = await fetch('http://localhost:4000/events');
+		if (!response.ok) throw new Error('Failed to fetch events');
+		const events = await response.json();
+		return { events };
+	} catch (error) {
+		console.error('Error fetching data:', error);
+		return { events: [] }; // Return empty data if there's an error
+	}
+};
